ad03b49a7ad3284d84aaa4eb72a3827b292be902
Nick Muir <83457561+NTmuir@users.noreply.github.com>
2022-08-01 14:54:36 +0100

    Update Gitt_drivecycle.jl

--- Julia/Gitt_drivecycle.jl
+++ Julia/Gitt_drivecycle.jl
@@ -1,6 +1,6 @@
 using PETLION, Plots, Statistics
 
-@time p = petlion(LCO;
+@time p = petlion(Chen2020;
 N_p = 10, # discretizations in the cathode
 N_s = 10, # discretizations in the separator
 N_n = 10, # discretizations in the anode
@@ -11,7 +11,7 @@ jacobian = :AD, # :AD (automatic-differenation) for convenience or :symbolic for
 )
 
 # Load the solution structure with an initial SOC of 0
-sol = solution()
+soly1 = solution()
 p.opts.outputs = (:t, :V, :I)
 p.opts.SOC = 0
 
@@ -20,34 +20,105 @@ p.opts.SOC = 0
 p.θ[:T₀] = p.θ[:T_amb] 
 # GITT: 20 1C pulses followed by 2 hour rests
 @time for i in 1:20
-    simulate!(sol, p, 3600/20, I=1)
-    simulate!(sol, p, 2*3600,  I=:rest)
+    simulate!(soly1, p, 3600/20, I=1)
+    simulate!(soly1, p, 2*3600,  I=:rest)
 end
 
-V_baseline = sol.V
-T_prime = plot(sol, :V)
+V_baseline = soly1.V
+T_prime = plot(soly1, :V)
 
 
-T = scatter(title="Average Sensistivty Temperature")
-T1 = scatter(title="Delta Sensistivty Temperature")
+Tx = scatter(title="Average Sensistivty Temperature")
+Tx1 = scatter(title="Delta Sensistivty Temperature")
 
-return1 = p.θ[:T₀]
+Vx = plot(title = "Terminal Voltage")
 
-# Temperature dependency
-for i in 233.15:0.1:373.15
-    p.θ[:T₀] = i
-    temp = round(i - 273.15)
-    @time for i in 1:20
-        simulate!(sol1, p, 3600/20, I=1)
-        simulate!(sol1, p, 2*3600,  I=:rest)
-    end
-    Delta_V1 = V_baseline - resize!(sol1.V,length(V_baseline))
+return1 = p.θ[:D_s]
+
+# Dependency
+for i in 1e-10:1e-11:10e-10
+    @time p.θ[:D_s] = i;
+    for k in 1:length(i)
+       sol = "Sol_$k "
+       print(sol) 
+       sol = solution()
+        p.opts.outputs = (:t, :V, :I)
+        p.opts.SOC = 0
+        @time for j in 1:20
+        simulate!(sol, p, 3600/20, I=1)
+        simulate!(sol, p, 2*3600,  I=:rest)
+        end
+    Delta_V1 = V_baseline - resize!(sol.V,length(V_baseline))
     Sens_Av1 = mean((Delta_V1/V_baseline).*100)
     Sens_max1 = maximum((Delta_V1/V_baseline).*100)
     Sens_min1 = minimum((Delta_V1/V_baseline).*100)
     Variation1 = Sens_max1 - Sens_min1
-    scatter!(T,(temp,Sens_Av1),xlabel = "Temperature (degC)",ylabel = "Average Variation (%)",  legend = false)
-    scatter!(T1,(temp,Variation1),xlabel = "Temperature (degC)",ylabel = "Average Variation (%)", legend = false)
+    scatter!(Tx,(i,Sens_Av1),xlabel = "Meh",ylabel = "Average Variation (%)",  legend = false)
+    scatter!(Tx1,(i,Variation1),xlabel = "Meh",ylabel = "Average Variation (%)", legend = false)
+    print(i) 
+    end
 end 
 
-p.θ[:T₀] = return1
+p.θ[:D_s] = return1
+
+return2 = p.θ[:D_n] 
+
+Tx2 = scatter(title="Average Sensistivty Temperature")
+Tx3 = scatter(title="Delta Sensistivty Temperature")
+
+# Dependency
+for i in 1e-10:1e-11:10e-10
+    @time p.θ[:D_n] = i;
+    for k in 1:length(i)
+       sol = "Sol1_$k "
+       print(sol) 
+       sol = solution()
+        p.opts.outputs = (:t, :V, :I)
+        p.opts.SOC = 0
+        @time for j in 1:20
+        simulate!(sol, p, 3600/20, I=1)
+        simulate!(sol, p, 2*3600,  I=:rest)
+        end
+    Delta_V1 = V_baseline - resize!(sol.V,length(V_baseline))
+    Sens_Av1 = mean((Delta_V1/V_baseline).*100)
+    Sens_max1 = maximum((Delta_V1/V_baseline).*100)
+    Sens_min1 = minimum((Delta_V1/V_baseline).*100)
+    Variation1 = Sens_max1 - Sens_min1
+    scatter!(Tx2,(i,Sens_Av1),xlabel = "Meh",ylabel = "Average Variation (%)",  legend = false)
+    scatter!(Tx3,(i,Variation1),xlabel = "Meh",ylabel = "Average Variation (%)", legend = false)
+    print(i) 
+    end
+end 
+
+p.θ[:D_s] = return2
+
+return3 = p.θ[:D_p] 
+
+Tx4 = scatter(title="Average Sensistivty Temperature")
+Tx5 = scatter(title="Delta Sensistivty Temperature")
+
+# Dependency
+@time for i in 1e-10:1e-11:10e-10
+    @time p.θ[:D_p] = i;
+    for k in 1:length(i)
+       sol = "Sol2_$k "
+       print(sol) 
+       sol = solution()
+        p.opts.outputs = (:t, :V, :I)
+        p.opts.SOC = 0
+        @time for j in 1:20
+        simulate!(sol, p, 3600/20, I=1)
+        simulate!(sol, p, 2*3600,  I=:rest)
+        end
+    Delta_V1 = V_baseline - resize!(sol.V,length(V_baseline))
+    Sens_Av1 = mean((Delta_V1/V_baseline).*100)
+    Sens_max1 = maximum((Delta_V1/V_baseline).*100)
+    Sens_min1 = minimum((Delta_V1/V_baseline).*100)
+    Variation1 = Sens_max1 - Sens_min1
+    scatter!(Tx4,(i,Sens_Av1),xlabel = "Meh",ylabel = "Average Variation (%)",  legend = false)
+    scatter!(Tx5,(i,Variation1),xlabel = "Meh",ylabel = "Average Variation (%)", legend = false)
+    print(i) 
+    end
+end 
+
+p.θ[:D_s] = return3
