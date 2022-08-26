02e92816e8cc3d07541da46cee44c5ca8a7afb09
Nick Muir <83457561+NTmuir@users.noreply.github.com>
2022-08-26 13:45:06 +0100

    git push

--- Julia/PSO_LM/PSO_tester_PETLION.jl
+++ Julia/PSO_LM/PSO_tester_PETLION.jl
@@ -1,20 +1,57 @@
-using Optim, PETLION, .Drivecycles
+using Optim, PETLION
 
 # Hard Limits
 max_itr = 10
 itr = 0
 
-x0 = [p.θ[:l_p]]
+x0 = p.θ[:l_p]
+
+y1 = 10
+
+y2 = 100
+
+ #Baseline GITT
+ p.θ[:T₀] = p.θ[:T_amb] 
+ soly_3 = solution()
+ p.opts.outputs = (:t, :V, :I)
+ p.opts.SOC = 1
+ p.bounds.V_min = 2.5
+ p.bounds.V_max = 4.2
+ # GITT: 20 1C pulses followed by 2 hour rests
+ @time for i in 1:20
+     simulate!(soly_3, p, y1, I=-0.25)
+     simulate!(soly_3, p, y2,  I=:rest)
+ end
+
+ V_baseline_3 = soly_3.V
 
 
-function f(x::Vector)
-    return (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
+function objective_function(x)
+    p.θ[:l_p] = x[1]
+    sol = "Sol_$x "
+    sol = solution()
+    p.opts.outputs = (:t, :V, :I)
+    p.opts.SOC = 1
+    p.bounds.V_min = 2.5
+    p.bounds.V_max = 4.2
+    for j in 1:20
+        simulate!(sol, p, y1, I=-0.25)
+        simulate!(sol, p, y2,  I=:rest)
+    end
+    Delta_V = V_baseline_3 - resize!(sol.V,length(V_baseline_3))    
+    if maximum(sol.V) > 5 || minimum(sol.V) < 0
+        NAN = zeros(length(V_baseline_3))
+        Delta_V = fill!(NAN,NaN)
+        println("$i is bad")
+    end
+    RMSE = sqrt.(sum.(Delta_V^.2) / length(Delta_V))
+    return RMSE
 end
 
-while itr < max_itr
+
     println(itr)
     
-    result = optimize(f,[0.0, 2.0], ParticleSwarm())
+    @show result = optimize(objective_function,[0.0, .0])
 
     Optim.minimizer(res)     
 
@@ -22,4 +59,3 @@ while itr < max_itr
 
     global itr +=1
     print(itr)
-end
