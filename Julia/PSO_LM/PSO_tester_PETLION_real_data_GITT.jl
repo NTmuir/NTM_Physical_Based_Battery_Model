0050e50e9897c02cad8310bb68c56af5280f3920
Nick Muir <83457561+NTmuir@users.noreply.github.com>
2022-09-19 19:49:15 +0100

    git push

--- Julia/PSO_LM/PSO_tester_PETLION_real_data_GITT.jl
+++ Julia/PSO_LM/PSO_tester_PETLION_real_data_GITT.jl
@@ -10,36 +10,52 @@ temperature = false, # temperature enabled or disabled
 jacobian = :AD, # :AD (automatic-differenation) for convenience or :symbolic for speed
 )
 
-case = 2
+x1 = OAT(1)[1]
+lb1 = OAT(1)[2]
+ub1 = OAT(1)[4]
+rand1 = OAT(1)[7]
 
-# PSO Outputs as optimal
-#= p.θ[:l_p] = 6.81538073229544e-5
-p.θ[:ϵ_p] = 0.33499104045541106
-p.θ[:l_n] = 8.519809670083905e-5
-p.θ[:k_n] = 2.0e-10
-p.θ[:c_max_p] = 51762.642078427125
-p.θ[:ϵ_n] = 0.2500172374812859 =#
+x2 = OAT(2)[1]
+lb2 = OAT(2)[2]
+ub2 = OAT(2)[4]
+rand2 = OAT(2)[7]
 
-            println("Case $case starting")
+x3 = OAT(3)[1]
+lb3 = OAT(3)[2]
+ub3 = OAT(3)[4]
+rand3 = OAT(3)[7]
 
-                x0 = OAT(case)[1]
-                lb = OAT(case)[2]
-                step = OAT(case)[3]
-                ub = OAT(case)[4]
-                name = OAT(case)[5]
-                unit = OAT(case)[6]
+x4 = OAT(4)[1]
+lb4 = OAT(4)[2]
+ub4 = OAT(4)[4]
+rand4 = OAT(4)[7]
+
+x5 = OAT(5)[1]
+lb5 = OAT(5)[2]
+ub5 = OAT(5)[4]
+rand5 = OAT(5)[7]
+
+x6 = OAT(6)[1]
+lb6 = OAT(6)[2]
+ub6 = OAT(6)[4]
 
 function objective_function(x)
-    p.θ[:ϵ_p] = x[1]
+    @show p.θ[:l_p] = x[1]
+    @show p.θ[:ϵ_p] = x[2]
+    @show p.θ[:l_n]= x[3]
+    @show p.θ[:k_n] = x[4]
+    @show p.θ[:c_max_p] = x[5]
+    @show p.θ[:ϵ_n] = x[6]
+
     sol = "Sol_$x"
     sol = solution()
-    @time for i in 1:20
+    for i in 1:20
         simulate!(sol, p, 720, I=-0.25)
         simulate!(sol, p, 4*3600,  I=:rest)
     end #Simulate the same response from the baseline
     
     if maximum(sol.V) > 5 || minimum(sol.V) < 0 #Plausability check to change anything to NaN 
-        NAN = zeros(length(V_baseline_3))
+        NAN = zeros(length(sol.V))
         Delta_V = fill!(NAN,NaN)
     end
 
@@ -66,11 +82,51 @@ end
 
 println("objective_function loaded in")
     
-@show result = optimize(objective_function,[lb lb; ub ub],PSO(options=Options(debug = true,store_convergence = true,iterations=50)))
+@show result = optimize(objective_function,[lb1 lb2 lb3 lb4 lb5 lb6; ub1 ub2 ub3 ub4 ub5 ub6],PSO(N=20,information = Information(x_optimum=[rand1,rand2,rand3,rand4,rand5,rand6]),options=Options(debug = true,store_convergence = true,time_limit=3600.0)))
 
-PSO_min = minimizer(result)[1]
+l_p_min = minimizer(result)[1]
+ϵ_p_min = minimizer(result)[2]
+l_n_min = minimizer(result)[3]
+k_n_min = minimizer(result)[4]
+c_max_p_min = minimizer(result)[5]
+ϵ_n_min = minimizer(result)[6]
 
-@show Error = ((x0 - PSO_min)/x0) * 100
+o = petlion(Chen2020;
+N_p = 10, # discretizations in the cathode
+N_s = 10, # discretizations in the separator
+N_n = 10, # discretizations in the anode
+N_r_p = 10, # discretizations in the solid cathode particles
+N_r_n = 10, # discretizations in the solid anode particles
+temperature = false, # temperature enabled or disabled
+jacobian = :AD, # :AD (automatic-differenation) for convenience or :symbolic for speed
+)
+
+#PSO profile check
+o.θ[:l_p] = l_p_min
+o.θ[:ϵ_p] = ϵ_p_min
+o.θ[:l_n]= l_n_min 
+o.θ[:k_n] = k_n_min
+o.θ[:c_max_p] = c_max_p_min
+o.θ[:ϵ_n] = ϵ_n_min
+
+        #Baseline GITT
+        o.θ[:T₀] = o.θ[:T_amb] 
+        soly_3 = solution()
+        o.opts.outputs = (:t, :V, :I)
+        o.opts.SOC = 1
+        o.bounds.V_min = 2.5
+        o.bounds.V_max = 4.2
+        # GITT: 20 1C pulses followed by 2 hour rests
+        @time for i in 1:20
+            simulate!(soly_3, o, 720, I=-0.25)
+            simulate!(soly_3, o, 4*3600,  I=:rest)
+        end
+
+        V_baseline_3 = soly_3.V
+        T_prime_3 = plot(soly_3, :V, title = "GITT")
+        Time_3 = soly_3.t
+        Current_3 = soly_3.I
+
 
 f_calls, best_f_value = convergence(result)
 
